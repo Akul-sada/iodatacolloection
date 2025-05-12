ceebb061716964b964838dccf074cc738298e446
Akul-sada <sadananda.sherigara@gmail.com>
2025-05-12 17:31:17 +0530

    define router which was not defined before

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -13,6 +13,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// Define the router
+const router = express.Router();
+
 app.get('/', (req, res) => {
     res.send(`'Hello World!'`);
 });
