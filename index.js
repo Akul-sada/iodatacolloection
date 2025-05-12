3299685e825fd0ce171793e367842feab86db82e
Akul-sada <sadananda.sherigara@gmail.com>
2025-05-12 16:52:17 +0530

    add port to the project and netlify for deployment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
+const serverless = require('serverless-http');
 const app = express();
 const port = 3000;
 const cors = require('cors');   
 app.use(cors()); // Enable CORS for all routes  
 
 app.use(cors({
-    origin: ['https://invisible-object.netlify.app/','https://copy-phoenix.netlify.app/','http://localhost:3000','https://jayalaxmisilks-bannanje.netlify.app/'] // Only allow requests from this domain
+    origin: ['https://invisible-object.netlify.app/','https://copy-phoenix.netlify.app/','http://localhost:3000/','https://jayalaxmisilks-bannanje.netlify.app/'] // Only allow requests from this domain
   }));
 
 app.use(express.json());
@@ -15,3 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send(`'Hello World!'`);
 });
+
+// Add this for local development
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
+
+
+module.exports.handler = serverless(app);
